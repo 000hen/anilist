6b40d83882de4a9d44e7aa6bd5217802db039062
000hen <git@muisnowdevs.one>
2026-09-16 01:08:21 +0800

    chore: bump version to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "one.muisnowdevs.apps.anilist"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
